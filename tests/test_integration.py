d8d2a0b27240d9577fe2b0a68015ddf36fbd543f
Oana Demian <oana.demian@gmail.com>
2025-10-16 16:13:57 +0100

    adds integration tests for when we set one tyre to the car

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -8,21 +8,20 @@ Initially we have no readings for any tyre
 def test_no_readings_for_tyres():
     car = Car()
     # front_left = Tyre("front left")
-    assert car.tyres["front left"].get_readings == []
+    assert car.tyres["front left"].get_readings() == []
     
 
 
 """
 Given a car
 When we add a tyre
-We see the latest reading for that tyre
+We see the reading for that tyre
 """
-# => "The latest reading for front left tyre was on 2025/08/30. The pressure was 34 PSI and the tread_depth was 5mm"
 
-# def test_when_we_add_a_tyre_get_latest_reading():
-#     car = Car()
-#     tyre_front_left = Tyre("front left")
-#     tyre_front_left.add_reading("34", "5", "2025/08/30")
-#     car.add_tyre_info(tyre_front_left)
-#     car.history()
-#     pass
+def test_when_we_add_a_tyre_gets_the_reading():
+    car = Car()
+    tyre_front_left = Tyre("front left")
+    tyre_front_left.add_reading("34", "5", "2025/08/30")
+    car.add_tyre_info(tyre_front_left)
+    assert car.tyres["front left"].get_readings()[0] == {'pressure': '34', 'tread depth': '5', 'date': '2025/08/30'}
+    
